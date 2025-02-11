04f3a15c064c853a91db792e2dde838059f63ea6
Felipe Díaz <felipeadiazreyes@gmail.com>
2025-02-11 10:23:59 -0300

    a

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,13 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/components/login/login.component';
 import { authGuard } from './core/guards/auth.guard';
 import { HomeComponent } from './views/home/home.component';
+import { TestComponent } from './views/test/test.component';
 
 export const routes: Routes = [
+    {
+        path: "test",
+        component: TestComponent
+    },
     {
         path: "",
         redirectTo: "portal",
