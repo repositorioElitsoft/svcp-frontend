96ec5457e3a6d258b282dd524b563cbb514764b3
Felipe Duarte <felipe.duarte@elitsoft-chile.com>
2025-02-11 17:02:33 -0300

    cambios

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { CommonModule } from '@angular/common';
+import { BusquedaComponent } from '../../shared/components/busqueda/busqueda.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatSidenavModule, BusquedaComponent], // Agrega CommonModule para Tailwind
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
 
+
+  ejecutarBusqueda(query: string) {
+    console.log('Buscando:', query);
+    // Aquí llamas al servicio para obtener resultados del backend
+  }
 }
