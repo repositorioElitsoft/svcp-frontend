4b0cb68602f42a898cfe366b5458c0cfa0890002
Felipe Duarte <felipe.duarte@elitsoft-chile.com>
2025-02-12 17:08:33 -0300

    cambios

--- src/app/shared/components/perfil/perfil-dialog/perfil-dialog.component.ts
+++ src/app/shared/components/perfil/perfil-dialog/perfil-dialog.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { MatIcon } from '@angular/material/icon';
 import { TranslateModule } from '@ngx-translate/core';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-perfil-dialog',
   standalone: true,
-  imports: [MatIcon, TranslateModule],
+  imports: [MatIcon, TranslateModule, MatCardModule],
   templateUrl: './perfil-dialog.component.html',
   styleUrls: ['./perfil-dialog.component.css']
 })
--- src/app/shared/components/perfil/perfil.component.ts
+++ src/app/shared/components/perfil/perfil.component.ts
@@ -13,12 +13,18 @@ import { PerfilDialogComponent } from './perfil-dialog/perfil-dialog.component';
 export class PerfilComponent {
   constructor(private dialog: MatDialog) { }
 
-  openPerfilDialog() {
-    this.dialog.open(PerfilDialogComponent, {
+  openPerfilDialog(event: MouseEvent) {
+    const dialogRef = this.dialog.open(PerfilDialogComponent, {
       width: '400px',
       height: '300px',
-      panelClass: ['custom-dialog-container', 'dark:bg-neutral-800']
+      panelClass: ['custom-dialog-container', 'dark:bg-neutral-800'],
+      position: {
+        top: `${event.clientY + 10}px`,           // Posición vertical hacia abajo (con un margen de 10px)
+        left: `${event.clientX - 420}px`          // Posición horizontal hacia la izquierda (restando el ancho del diálogo)
+      }
     });
   }
+
+
 
 }
