64e6e07cbcd51b99a22286c4ddd2f0e12094df36
Felipe Duarte <felipe.duarte@elitsoft-chile.com>
2025-02-13 10:43:29 -0300

    cambios

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,28 +1,40 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { CommonModule } from '@angular/common';
+import Swiper from 'swiper';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
 import { BusquedaComponent } from '../../shared/components/busqueda/busqueda.component';
 import { ThemeToggleComponent } from '../../shared/components/theme-toggle/theme-toggle.component';
 import { PerfilComponent } from '../../shared/components/perfil/perfil.component';
 import { CalendarioEventoComponent } from '../../shared/components/calendario-evento/calendario-evento.component';
 import { TituloParametrizadoComponent } from '../../shared/components/titulo-parametrizado/titulo-parametrizado.component';
 import { CarouselHomeComponent } from '../../shared/components/carousel-home/carousel-home.component';
-import SwiperCore from 'swiper';
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
 
-SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
+// Configuración de Swiper
+Swiper.use([Navigation, Pagination, Scrollbar, A11y]);
+
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, MatSidenavModule, BusquedaComponent, ThemeToggleComponent,
-    PerfilComponent, CalendarioEventoComponent, TituloParametrizadoComponent, CarouselHomeComponent],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  imports: [
+    CommonModule,
+    MatSidenavModule,
+    BusquedaComponent,
+    ThemeToggleComponent,
+    PerfilComponent,
+    CalendarioEventoComponent,
+    TituloParametrizadoComponent,
+    CarouselHomeComponent
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA], // Agregar esto para evitar errores con Swiper
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
 
-  images: string[] = ["sliderhome1.png", "sliderhome2.png", "sliderhome3.png"]
+  images: string[] = [
+    "sliderhome1.png", "sliderhome2.png", "sliderhome3.png", "sliderhome4.png"
+  ];
 
 
   ejecutarBusqueda(query: string) {
@@ -30,9 +42,8 @@ export class HomeComponent {
     // Aquí llamas al servicio para obtener resultados del backend
   }
 
-
   getImageRoute(image: string) {
-    return `assets/${image}`
+    return `assets/${image}`;  // Asegúrate de que las imágenes están en la carpeta 'assets/'
   }
 
 }
