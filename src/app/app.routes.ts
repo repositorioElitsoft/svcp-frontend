2fab1b5d496f41c8f096464da07ade31679867c6
Marco Rodriguez <mrodriguez2626@gmail.com>
2025-02-26 13:14:42 -0300

    mod: se modifica el modulo de login para que conecte con el backend

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -21,7 +21,7 @@ export const routes: Routes = [
     },
     {
         path: "portal",
-        canActivate: [/*authGuard*/],
+        canActivate: [authGuard],
         children: [
             {
                 path: "",
