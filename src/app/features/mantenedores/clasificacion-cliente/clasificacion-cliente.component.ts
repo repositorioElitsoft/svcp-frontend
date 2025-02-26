ceaea96014c088f4b8b3e37f2359987e346331ba
Felipe Duarte <felipe.duarte@elitsoft-chile.com>
2025-02-26 16:53:43 -0300

    cambios

--- src/app/features/mantenedores/clasificacion-cliente/clasificacion-cliente.component.ts
+++ src/app/features/mantenedores/clasificacion-cliente/clasificacion-cliente.component.ts
@@ -207,25 +207,23 @@ export class ClasificacionClienteComponent implements OnInit {
     }).subscribe((data: PagedResponse<ClasificacionCliente[]>) => {
       console.log("Datos recibidos:", data);
 
-      this.pageNumber = data.pageNumber
-      this.totalPages = data.totalPages
+      this.pageNumber = data.pageNumber;
+      this.totalPages = data.totalPages;
       this.pageSize = data.pageSize;
       this.totalElements = data.totalElements;
 
       this.dataSource = data.content.flat();
       if (data.content.length > 0) {
-        this.displayedColumns = Object.keys(data.content[0]);
+        this.displayedColumns = this.getDisplayedColumns(data.content[0]);
       }
       this.cdr.detectChanges();
     });
   }
 
-  // transformarNombreColumna(columna: string): string {
-  //   return columna
-  //     .replace(/([A-Z])/g, ' $1')
-  //     .replace(/^./, str => str.toUpperCase())
-  //     .trim();
-  // }
+  private getDisplayedColumns(row: any): string[] {
+    return Object.keys(row).filter(key => key !== 'id'); // Ocultamos la columna 'id'
+  }
+
 
 }
 
