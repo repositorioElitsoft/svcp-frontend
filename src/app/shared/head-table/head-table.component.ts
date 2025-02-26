0b97701a90cd8c492f26153b8730aa21ff341507
Felipe Duarte <felipe.duarte@elitsoft-chile.com>
2025-02-26 12:27:15 -0300

    cambios

--- src/app/shared/head-table/head-table.component.ts
+++ src/app/shared/head-table/head-table.component.ts
@@ -1,20 +1,15 @@
 import { Component } from '@angular/core';
-import { BusquedaComponent } from '../components/busqueda/busqueda.component';
 import { ThemeToggleComponent } from '../components/theme-toggle/theme-toggle.component';
 import { PerfilComponent } from '../components/perfil/perfil.component';
 
 @Component({
   selector: 'app-head-table',
   standalone: true,
-  imports: [BusquedaComponent, ThemeToggleComponent, PerfilComponent],
+  imports: [ThemeToggleComponent, PerfilComponent],
   templateUrl: './head-table.component.html',
   styleUrl: './head-table.component.css'
 })
 export class HeadTableComponent {
 
-  ejecutarBusqueda(query: string) {
-    console.log('Buscando:', query);
-    // Aquí llamas al servicio para obtener resultados del backend
-  }
 
 }
