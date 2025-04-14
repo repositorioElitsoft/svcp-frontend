7dee51498c0c77988a3f04a0e0d819f17b148363
Felipe Duarte <felipe.duarte@elitsoft-chile.com>
2025-04-14 15:49:01 -0400

    cambios

--- src/app/features/mantenedores/trabajo/trabajo.component.ts
+++ src/app/features/mantenedores/trabajo/trabajo.component.ts
@@ -195,11 +195,7 @@ export class TrabajoComponent implements OnInit {
     const fieldMapping: { [key: string]: string } = {
       'id': 'id',
       'descripcionTrabajo': 'descripcionTrabajo',
-      'fechaCreacion': 'fechaCreacion',
-      'fechaModificacion': 'fechaModificacion',
-      'estado': 'estado',
-      'usuarioCreacion': 'usuarioCreacion',
-      'usuarioModificacion': 'usuarioModificacion'
+
     };
 
     // Obtener el campo de ordenamiento mapeado o usar el nombre de la columna original si no existe mapeo
