64c2852c2964975b56235b93e98a06031f6a7f76
Felipe Díaz <felipeadiazreyes@gmail.com>
2025-04-22 14:40:15 -0400

    hasta 18

---
+++ src/app/shared/components/sub-forms/show-cliente/show-cliente.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDialogRef, MAT_DIALOG_DATA, MatDialogContent, MatDialogActions, MatDialogClose } from '@angular/material/dialog';
+import { TranslateModule } from '@ngx-translate/core';
+import { TituloDialogoComponent } from "../../titulo-dialogo/titulo-dialogo.component";
+import { Cliente } from '../../../../core/models/cliente.model';
+
+
+
+@Component({
+    selector: 'app-show-cliente',
+    templateUrl: './show-cliente.component.html',
+    standalone: true,
+    imports: [
+        CommonModule,
+        MatButtonModule,
+        MatIconModule,
+        MatDialogContent,
+        MatDialogActions,
+        MatDialogClose,
+        TranslateModule,
+        TituloDialogoComponent
+    ]
+})
+export class ShowClienteComponent implements OnInit {
+    readonly dialogRef = inject(MatDialogRef<ShowClienteComponent>);
+    readonly data = inject<any>(MAT_DIALOG_DATA);
+
+    selectedFile: File | null = null;
+    fileUrl: string | null = null;
+    cliente: any | undefined;
+    showLocaciones = false;
+
+    constructor() { }
+
+    ngOnInit(): void {
+        // Inicializar el cliente desde los datos del diálogo
+        if (this.data && this.data.object) {
+            this.cliente = this.data.object;
+            console.log("show cliente: ", this.cliente);
+        }
+    }
+
+    toggleLocaciones(): void {
+        this.showLocaciones = !this.showLocaciones;
+    }
+
+    aceptar(): void {
+        this.dialogRef.close(true);
+    }
+}
