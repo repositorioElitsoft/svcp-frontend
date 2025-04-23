6184857647952593ab18f70dec05af90aa2f2121
Felipe Duarte <felipe.duarte@elitsoft-chile.com>
2025-04-23 17:49:57 -0400

    add colores

--- src/app/shared/components/opciones-mantenedor/opciones-mantenedor.component.ts
+++ src/app/shared/components/opciones-mantenedor/opciones-mantenedor.component.ts
@@ -28,9 +28,9 @@ import { TranslateModule } from "@ngx-translate/core"
   <!-- Contenedor del botón de eliminar -->
   <div class="flex justify-end w-full sm:w-auto">
     <button [disabled]="!hasSelection"
-            class="elitsoft-btn-icon flex items-center justify-center gap-2"
+            class="flex items-center justify-center gap-2"
             (click)="onEliminar()">
-      <mat-icon>delete</mat-icon>
+      <span class="material-symbols-outlined">delete</span>
     </button>
   </div>
 
